f4972bc42406ef835ad3e73e1ce8016e22a2392a
Muskan Joshi <m8sk2nj@gmail.com>
2025-06-13 18:22:25 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function Portfolio() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-4xl font-bold text-blue-700">Muskan Joshi Portfolio is Working 🎉</h1>
+    </div>
+  );
+}
